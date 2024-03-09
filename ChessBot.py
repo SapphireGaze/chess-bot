60e3632926f809099afbcbf91758c7e46aa2cb7b
SapphireGaze <jasminehuang2048@outlook.com>
2024-03-08 17:06:10 -0800

    Min Max Algo & Alpha Beta Pruning

--- ChessBot.py
+++ ChessBot.py
@@ -1,5 +1,5 @@
-import chess as ch # AKA python-chess
 import random
+import chess as ch
 
 class Bot:
     # Properties:
@@ -24,15 +24,99 @@ class Bot:
         self.Board = Board
         self.Color = Color
         self.max_depth = max_depth
+
+        self.piece_values = {
+            ch.PAWN : 1.0,
+            ch.ROOK : 5.1,
+            ch.BISHOP : 3.33,
+            ch.KNIGHT : 3.2,
+            ch.QUEEN : 8.8,
+            ch.KING : 100.0
+        } # based on Hans Berliner System
     
     def getBestMove(self):
-        best_move = random.choice(list(self.Board.legal_moves))
+        best_move, best_value = self.search(1, float("-inf"), float("inf"))
+        print(best_move, best_value)
+
         return best_move
 
     # Move score based on current board state
     def evaluate(self):
-        pass
+        score = 0.0
+
+        # Iterate through entire board, sum up piece values of each square
+        for i in range(64):
+            score += self.evalSquareValue(ch.SQUARES[i])
+        
+        score += random.random() * 0.001
+
+        return score
+
+    def evalSquareValue(self, square):
+        # Takes a square as input and returns
+        # the corresponding Hans Berliner's
+        # system value of the piece atop it
+        
+        piece_type = self.Board.piece_type_at(square)
+        if piece_type:
+            piece_value = self.piece_values[piece_type]
+            return piece_value if (self.Board.color_at(square) == self.Color) else -piece_value
+        
+        return 0
 
     # Recursive search function
-    def search(self, depth):
-        pass
+    def search(self, depth, alpha, beta):
+        # Reached max depth of search or no possible moves
+        if (depth == self.max_depth or self.Board.legal_moves.count() == 0):
+            return None, self.evaluate()
+
+        # - Bot plays every odd turn (odd depth)
+        # - When bot plays, it wants to maximize its score
+        # - When it's human's turn, it wants to minize their score
+        is_maximizing = (depth % 2 != 0)
+
+        if is_maximizing:
+            best_value = float("-inf") 
+            best_move = None
+            
+            for move in self.Board.legal_moves:
+                self.Board.push(move)
+
+                _, value_candidate = self.search(depth + 1, alpha, beta)
+
+                if value_candidate > best_value:
+                    best_move = move
+                    best_value = value_candidate
+
+                if best_value > beta:
+                    self.Board.pop()
+                    break
+
+                alpha = max(alpha, best_value)
+
+                self.Board.pop()
+
+            return best_move, best_value
+
+        elif not is_maximizing:
+            best_value = float("inf")
+            best_move = None
+
+            for move in self.Board.legal_moves:
+                self.Board.push(move)
+
+                _, value_candidate = self.search(depth + 1, alpha, beta)
+
+                if value_candidate < best_value:
+                    best_move = move
+                    best_value = value_candidate
+
+                if best_value < alpha:
+                    self.Board.pop()
+                    break
+
+                beta = min(beta, best_value)
+
+                self.Board.pop()
+
+            return best_move, best_value
